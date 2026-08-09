b9abc04e1b3e4f24ce6363a85092e918d174b6c8
ugoezeogbodo-wq <ugoezeogbodo@gmail.com>
2026-08-09 20:20:25 +0100

    Added the check answer function. It verifies whether the input ans is correct or not and prints the corresponding message. Then it either deducts or adds a point to your status. It wont add if you are already at 10 or got it wrong the first time. Additionaly, no deduction will occur if your status is 0. Next, if you got the question right, it updates your due time. Changed the status info into a dictionary, aswell!

--- Nihongo_Charater_Practice.py
+++ Nihongo_Charater_Practice.py
@@ -2,29 +2,35 @@ import Character_Dictionary as CD
 import datetime as dt
 import random
 
-stage1= dt.timedelta(minutes=1)
-stage2= dt.timedelta(minutes=5)
-stage3= dt.timedelta(minutes=15)
-stage4= dt.timedelta(days=1)
-stage5= dt.timedelta(days=2)
-stage6= dt.timedelta(days=3)
-stage7= dt.timedelta(days=5)
-stage8= dt.timedelta(days=7)
-stage9= dt.timedelta(days=14)
-stage10=dt.timedelta(days=21)
+stages = {
+    0: dt.timedelta(seconds=5),
+    1: dt.timedelta(minutes=1),
+    2: dt.timedelta(minutes=5),
+    3: dt.timedelta(minutes=5),
+    4: dt.timedelta(days=1),
+    5: dt.timedelta(days=2),
+    6: dt.timedelta(days=3),
+    7: dt.timedelta(days=5),
+    8: dt.timedelta(days=7),
+    9: dt.timedelta(days=14),
+    10: dt.timedelta(days=21)}
+
 
 queue = []
 now = dt.datetime.now()
+target = []
 
 def status_check():
     global queue, now
+    now = dt.datetime.now()
     queue = []
     for nihon in CD.hiragana_dataset:
-        if nihon["due_time"] == None or nihon["due_time"] > now :
+        if nihon["due_time"] == None or nihon["due_time"] <= now :
             queue.append(nihon)
     
 
 def load_new_deck():
+    global target
     status_check()
     if queue == []:
         print("You're all caught up! Good Job!")
@@ -37,10 +43,35 @@ def load_new_deck():
         others = random.sample(disposable, k= 3)
         final = [target] + others
         random.shuffle(final)
-        print(final)
         print("What is the romaji equivalent of", target["character"], "?")
         for char in final:
             print(char["romaji"])
+        check_answer()
 
+def check_answer():
+    global target, now
+    now = dt.datetime.now()
+    status = False
+    tries = 0
+    while status == False:
+        user_ans = input()
+        if user_ans == target["romaji"]:
+            print("Goodjob!")
+            if target["status"] != 10 and target != 1:
+                target["status"] += 1
+            target["due_time"] = now + stages[target["status"]]
+            status = True
+        else:
+            if tries != 1:
+                print("OHNO! Remember", target["mnemonic"])
+            if tries != 1:
+                if target["status"] != 0:
+                    target["status"] -= 1
+            tries = 1
+        load_new_deck()
+
+        
+            
 load_new_deck()
-user_ans = input()
+check_answer()
+
